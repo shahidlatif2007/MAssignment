55d15c662865e6883a754cba9d8613f1e7f89876
Shahid Latif <shahid.latif@huawei.com>
2019-10-13 21:25:39 +0400

    Initial Commit

---
+++ Service Center/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Service Center
+//
+//  Created by Shahid Latif on 2019/10/13.
+//  Copyright © 2019 MOHRE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
